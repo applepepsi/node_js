b5f5017a786d479e3385d1e5d4a4c5ce315191c9
applepepsi <dlaejrtnz@gmail.com>
2023-01-28 02:46:56 +0900

    에러알림 추가

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,7 +22,19 @@ const id=document.querySelector("#id"),
             body: JSON.stringify(req),
         })
         .then((res)=> res.json())
-        .then(console.log);
+        .then((res)=>{
+            if (res.success)
+            {
+                location.href="/";
+            }
+            else
+            {
+                alert(res.msg);
+            }
+        })
+        .catch((err)=>{
+            console.err(new Error("로그인 중 에러 발생"));
+        });
     }
     
 
